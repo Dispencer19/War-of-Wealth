a9ce033a3e19d0340ec5d12413d2836650daa6db
Derek-on-top <derekddzhang@gmail.com>
2026-02-03 12:23:27 -0800

    Player Camera Movement
    spencer, can you change the sensitivity to 400, 400 and orientation (transform) in the inspection for the player cam script. In the inspection again in the MainCam, set it to the CameraPos

---
+++ War_Of_Wealth_v0/Assets/CamPos.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPos : MonoBehaviour
+{
+    public Transform cameraPosition;
+    private void Update(){
+        transform.position = cameraPosition.position;
+    }
+}
