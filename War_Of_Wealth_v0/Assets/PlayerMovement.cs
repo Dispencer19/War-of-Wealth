d22fd3f6724aeab57e8592268250beb4afb7d81c
Derek-on-top <derekddzhang@gmail.com>
2026-02-04 11:16:01 -0800

    movement

---
+++ War_Of_Wealth_v0/Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movemoent")]
+    public float moveSpeed;
+    public float groundDrag;
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public Transform orientation;
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update(){
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        MyInput();
+        if (grounded)
+            rb.linearDamping = groundDrag;
+        else
+            rb.linearDamping = 0;
+    }
+    
+    private void FixedUpdate(){
+        MovePlayer();
+    }
+
+    private void MyInput(){
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void MovePlayer(){
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+}
